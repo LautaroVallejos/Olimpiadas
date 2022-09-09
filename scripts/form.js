463e0ff183f0d2e99b9f444d159980dc5af81f08
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-09-09 20:29:40 -0300

    form.js actualizado

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,9 @@
 function sendForm(){
-    var name = document.getElementById('fullname')
+    var fullname = document.getElementById('fullname').value;
+    var pecho = document.getElementById('pecho').checked.value;
+    var crol = document.getElementById('crol').checked.value;
+    var espalda = document.getElementById('espalda').checked.value;
+    var mariposa = document.getElementById('mariposa').checked.value;
+
+    console.log(fullname, pecho, crol, espalda, mariposa);
 }
